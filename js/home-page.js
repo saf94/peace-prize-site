b7dbc92646832becbc9cd670ef3a8cfb4d3fb82b
Safeer Hayat <saf94@users.noreply.github.com>
2021-02-24 19:25:34 +0000

    complete desktop version

--- js/home-page.js
+++ js/home-page.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 // ==============
 // SECTION 1
 // ==============
@@ -5,25 +7,31 @@
 gsap.from('.section-1 .img-wrapper', {
 	scrollTrigger: {
 		trigger: '.section-1',
+		start: "top 70%",
 	},
 	duration: 1.5,
 	x: -500,
+	opacity: 0,
 })
 
 gsap.from('.section-1 .bg-circle', {
 	scrollTrigger: {
 		trigger: '.section-1',
+		start: "top 70%",
 	},
 	duration: 1.5,
 	y: 200,
+	opacity: 0,
 })
 
 gsap.from('.section-1 .content-wrapper', {
 	scrollTrigger: {
 		trigger: '.section-1',
+		start: "top 70%",
 	},
 	duration: 1.5,
 	x: 500,
+	opacity: 0,
 })
 
 // ==============
@@ -33,85 +41,100 @@ gsap.from('.section-1 .content-wrapper', {
 gsap.from('.section-2 .img-wrapper', {
 	scrollTrigger: {
 		trigger: '.section-2',
+		start: "top 70%",
 	},
 	duration: 1.5,
 	x: -500,
+	opacity: 0,
 })
 
 gsap.from('.section-2 .bg-circle', {
 	scrollTrigger: {
 		trigger: '.section-2',
+		start: "top 70%",
 	},
 	duration: 1.5,
 	y: 200,
+	opacity: 0,
 })
 
 gsap.from('.section-2 .content-wrapper', {
 	scrollTrigger: {
 		trigger: '.section-2',
+		start: "top 70%",
 	},
 	duration: 1.5,
 	x: 500,
+	opacity: 0,
 })
 
 // ==============
 // PREVIOUS WINNERS
 // ==============
 
-const yearButton = document.getElementsByClassName("2019")[0]
-let toggleShow = false
+gsap.to(
+	'.previous-winners .winner-2018, .winner-2017, .winner-2016, .winner-2015, .winner-2014', 
+	{
+	scrollTrigger: {
+		trigger: '.previous-winners',
+	},
+	y: 100,
+	duration: 0,
+})
+
+gsap.to('.previous-winners .winner-2019', {
+	scrollTrigger: {
+		trigger: '.previous-winners',
+	},
+	css: { 
+		display: "flex",
+		position: "static",
+		opacity: "1",
+	},
+	duration: 0.6,
+})
+
+const yearButtons = document.getElementsByClassName("yearTitle")
+let year;
 
 const tlPrevWinner = gsap.timeline({
 	defaults: { duration: 0.6 },
 });
 
-yearButton.addEventListener("click", function(e) {
-	const year = e.target.innerText
-	console.log(e.target.innerText)
-	toggleShow = !toggleShow
+for (const yearButton of yearButtons) {
+	yearButton.addEventListener("click", function(e) {
+		const prevYear = year || "2019"
+		year = e.target.innerText
+	
+		if (prevYear === year) return
 
-	if (toggleShow) {
-
-		tlPrevWinner.to(`.previous-winners .winner-${year} .winner-wrapper`, {
-			css: { maxWidth: "70%" },
-			duration: 0,
-		})
-
-		tlPrevWinner.to(`.previous-winners .winner-${year} .winner-details`, {
-			css: { display: "block" },
-			duration: 0.2,
+		for (const button of yearButtons) {
+			if (year == button.innerText) {
+				button.classList.add("active")
+			} else {
+				button.classList.remove("active")
+			}
+		}
+	
+		tlPrevWinner.to(`.previous-winners .winner-${prevYear}`, {
+			opacity: 0,
+			y: -100,
+			// duration: 1,
 		})
 		
-		tlPrevWinner.to(`.previous-winners .winner-${year} .winner-wrapper`, {
-			css: { backgroundColor: "rgba(255, 255, 255, 0.15)" },
-			duration: 0.5,
+		tlPrevWinner.to(`.previous-winners .winner-${year}`, {
+			css: { 
+				opacity: "1",
+				transform: "translate(0, 0)"
+			},
+			// duration: 1,
 		})
 
-		tlPrevWinner.to(`.previous-winners .winner-${year} .winner-details`, {
-			css: { opacity: 1 },
-		})
-
-	} else {
-
-		tlPrevWinner.to(`.previous-winners .winner-${year} .winner-details`, {
-			css: { opacity: 0 },
-		})
-
-
-		tlPrevWinner.to(`.previous-winners .winner-${year} .winner-wrapper`, {
-			css: { backgroundColor: "rgba(255, 255, 255, 0)" },
-			duration: 0.5,
-		})
-
-		tlPrevWinner.to(`.previous-winners .winner-${year} .winner-details`, {
-			css: { display: "none" },
-			duration: 0.2,
-		})
-
-		tlPrevWinner.to(`.previous-winners .winner-${year} .winner-wrapper`, {
-			css: { maxWidth: "none" },
+		tlPrevWinner.to(`.previous-winners .winner-${prevYear}`, {
+			y: 100,
 			duration: 0,
 		})
-	}
-})
+
+	})	
+};
 
