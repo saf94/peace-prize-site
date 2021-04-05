ea0465e59a7f23a71d2048cf6d3796fc6079d9fd
Safeer Hayat <saf94@users.noreply.github.com>
2021-04-05 16:24:17 +0100

    create header

--- header.php
+++ header.php
@@ -8,12 +8,11 @@
  *
  * @package peace_prize
  */
-
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -23,37 +22,34 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'peace-prize' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e(
+     'Skip to content',
+     'peace-prize'
+ ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$peace_prize_description = get_bloginfo( 'description', 'display' );
-			if ( $peace_prize_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $peace_prize_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'peace-prize' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+			<i class="mobile-nav fa fa-bars"></i>
+		<div class="navbar">
+		<a href="/">
+			<p>Home</p>
+		</a>
+		<a href="/about-us">
+			<p>About Us</p>
+		</a>
+		<div class="site-logo">
+ 			<img class="image-1" src="<?php bloginfo(
+        'template_url'
+    ); ?>/images/shape.png"/>
+ 			<img class="image-2" src="<?php bloginfo(
+        'template_url'
+    ); ?>/images/award.png"/>
+		</div>
+		<a href="/previous-winners">
+			<p>Previous Winners</p>
+		</a>
+		<a href="/nominate-winner">
+			<p>Nominate a Winner</p>
+		</a>
+		</div>
+		
 	</header><!-- #masthead -->
